2ca03bf8baafead36915e401b88d5726611ad037
Alan <14139491+alan852@users.noreply.github.com>
2023-02-11 16:21:01 +0000

    1. Add function to exclude categories 2. Minor amendment on format 3. Bug fixes

--- src/firefly_iii.py
+++ src/firefly_iii.py
@@ -4,9 +4,9 @@ from datetime import datetime
 from typing import Iterator, TypedDict
 
 import requests
-from jsonpath_ng import parse
+from jsonpath_ng.ext import parse
 
-from cal_unit_cost import cal_unit_cost
+from cal_unit_cost import cal_unit_cost, UnitCost
 from env import ENV, DEFAULT
 
 
@@ -15,25 +15,39 @@ class Transaction(TypedDict):
     date: datetime
 
 
+class Item(TypedDict):
+    name: str
+    size: float
+    unit: str
+    cost: float
+    store: str
+    date: datetime
+    unit_cost: UnitCost
+
+
 url: str = 'https://{domain}/api/v1/search/transactions'
-jsonpath_desc: str = '$.data[*].attributes.transactions[*]'
+jsonpath_desc: str = "$.data[*].attributes.transactions[*]"
 jsonpath_total_pages: str = '$.meta.pagination.total_pages'
-required_fields: str = ['description', 'date']
+required_fields: str = ['description', 'date', 'category_name']
 
 domain: str = None
 token: str = None
 desc_regex: str = None
+excl_cat = None
 
 
 def config_env():
-    global domain, token, desc_regex
+    global domain, token, desc_regex, excl_cat
     if domain is None:
         domain = os.environ.get(ENV.FIREFLY_III_DOMAIN)
     if token is None:
         token = os.environ.get(ENV.FIREFLY_III_TOKEN)
     if desc_regex is None:
-        desc_regex = os.environ.get(ENV.FIREFLY_III_TRAN_DESC_REGEX)\
+        desc_regex = os.environ.get(ENV.FIREFLY_III_TRAN_DESC_REGEX) \
             .replace("{units}", os.environ.get(ENV.UNITS, DEFAULT.UNITS))
+    if excl_cat is None:
+        excl_cat = os.environ.get(ENV.FIREFLY_III_EXCLUDED_CATEGORIES)
+        excl_cat = {_.upper() for _ in excl_cat.split(',') if _ != ''} if excl_cat is not None and excl_cat != '' else set()
 
 
 def retrieve_transactions(query: str, page: int = 1) -> Iterator[Transaction]:
@@ -43,25 +57,19 @@ def retrieve_transactions(query: str, page: int = 1) -> Iterator[Transaction]:
         params={'query': query, 'page': page},
         headers={'Authorization': f'Bearer {token}'}
     )
-    filtered_transactions = [
+    txn_with_required_fields = [
         {
             key: datetime.strptime(_.value[key][:-3] + _.value[key][-2:], '%Y-%m-%dT%H:%M:%S%z')
             if key == 'date' else _.value[key] for key in required_fields
         }
         for _ in parse(jsonpath_desc).find(response.json())
     ]
-    yield from filtered_transactions
+    yield from filter(lambda transaction: transaction['category_name'].upper() not in excl_cat, txn_with_required_fields)
     if page != parse(jsonpath_total_pages).find(response.json())[0].value:
         yield from retrieve_transactions(query, page=page + 1)
 
 
-cnv = {
-    'G': {'factor': 1.0 / 1000.0, 'to': 'KG'},
-    'ML': {'factor': 1.0 / 1000.0, 'to': 'L'}
-}
-
-
-def digest_transaction(transaction: Transaction):
+def digest_transaction(transaction: Transaction) -> Item:
     regex = rf'{desc_regex}'
     match = re.search(regex, transaction['description'], re.IGNORECASE)
     item = {'name': match.group('name'), 'size': float(match.group('size')),
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 from dotenv import find_dotenv, load_dotenv
 from telegram import Update
+from telegram.constants import ParseMode
 from telegram.error import InvalidToken
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes, ApplicationHandlerStop
 
@@ -28,11 +29,14 @@ logger.addHandler(fileHandler)
 
 admin_chat_id = os.environ.get(ENV.ADMIN_CHAT_ID)
 admin_chat_id = {int(admin_chat_id)} if admin_chat_id is not None and admin_chat_id != '' else set()
-allowed_chat_ids = {int(_) for _ in os.environ.get(ENV.ALLOWED_CHAT_IDS).split(',') if _ != ''}
+allowed_chat_ids = os.environ.get(ENV.ALLOWED_CHAT_IDS)
+allowed_chat_ids = {int(_) for _ in allowed_chat_ids.split(',') if _ != ''} \
+    if allowed_chat_ids is not None and allowed_chat_ids != '' else set()
 allowed_chat_ids |= admin_chat_id
 logger.info(f'Allowed Chat IDs: {allowed_chat_ids}')
 
 error_message = os.environ.get(ENV.TELEGRAM_BOT_ERROR_MESSAGE, DEFAULT.TELEGRAM_BOT_ERROR_MESSAGE)
+empty_result_message = os.environ.get(ENV.TELEGRAM_BOT_EMPTY_RESULT_MESSAGE, DEFAULT.TELEGRAM_BOT_EMPTY_RESULT_MESSAGE)
 
 
 async def message_response(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -41,7 +45,7 @@ async def message_response(update: Update, context: ContextTypes.DEFAULT_TYPE):
         try:
             unit_cost = cal_unit_cost_from_str(update.message.text)
             if unit_cost is not None:
-                response_text = f'{unit_cost["cost"]}/{unit_cost["unit"]}'
+                response_text = f'{unit_cost["cost"]:.2f}/{unit_cost["unit"]}'
             else:
                 items = list()
                 unknown = list()
@@ -52,11 +56,14 @@ async def message_response(update: Update, context: ContextTypes.DEFAULT_TYPE):
                         logger.error(f'{e} (Ref: {transaction})')
                         unknown.append(transaction)
                 items = sorted(items, key=lambda i: i['unit_cost']['cost'])
-                response_text = '\r\n'.join(f'{_["name"]} {_["size"]:.0f}{_["unit"]}, '
+                response_text = '\r\n'.join(f'• {_["name"]} {_["size"]:.0f}{_["unit"]}, '
                                             f'{_["unit_cost"]["cost"]:.2f}/{_["unit_cost"]["unit"]}'
                                             f' on {_["date"].strftime("%d/%m/%Y")} at {_["store"]}'
                                             for _ in items)
-                response_text += '\r\nUnkown Format\r\n' + '\r\n'.join(f'{_["description"]}' for _ in unknown)
+                if len(unknown) != 0:
+                    response_text += '\r\n\r\nUnknown Format\r\n' + '\r\n'.join(f'• {_["description"]}' for _ in unknown)
+                response_text = response_text if response_text is not None and response_text != '' \
+                    else empty_result_message
         except Exception as e:
             logger.error(e)
             response_text = error_message
